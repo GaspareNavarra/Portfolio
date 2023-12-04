89e9348120ad79ab58a9ab9200a9fb4e4420ba8f
Gaspare Navarra <gasparenavarra2@gmail.com>
2023-12-04 16:02:34 +0100

    Added GN Logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,18 @@
 <template>
     <nav id="NavBar" class="navbar navbar-expand-lg navbar-dark bg-dark no-margin">
-        <i></i>
+        <img :src="Logo" class="img-size"/>
         <dropdown-language></dropdown-language>
     </nav>
 </template>
 <script>
 import DropdownLanguage from './DropdownLanguage.vue';
+import Logo from '../../public/img/Gn-Logo.png';
 export default {
-    components: { DropdownLanguage }
+    components: { DropdownLanguage },
+    data() {
+        return {
+            Logo: Logo
+        }
+    }
 }
 </script>

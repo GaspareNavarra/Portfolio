3b819e23c92224b7cbb7d418884536660a12b5f1
Gaspare Navarra <gasparenavarra2@gmail.com>
2023-12-18 17:47:52 +0100

    Graphics changes

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -5,9 +5,25 @@
             <div class="title2">Navarra</div>
         </div>
     </div>
+    <div id="SocialMedia">
+        <a id="Instagram" href="https://www.instagram.com/not_so_serious__gaspare/">
+            <i class="bx bxl-instagram instagram"></i>
+            <div class="social_tag">@not_so_seriuos__gaspare</div>
+        </a>
+        <div id="gMail">
+            <!-- <i class="bx bxl-gmail gmail"></i> -->
+            <img :src="GMAIL" class="gmail_size">
+            <div class="social_tag">gasparenavarra2@gmail.com</div>
+        </div>
+    </div>
 </template>
 <script>
+import GMAIL from '../../../public/img/gmail.png';
 export default {
-    
+    data() {
+        return {
+            GMAIL: GMAIL
+        }
+    }
 }
 </script>
